ed59159dbf619aeeda80b882109a611a91786244
melodymarana <melody.marana@gmail.com>
2023-02-07 19:01:53 +0700

    Updates

--- src/components/pageTitle/index.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types'
-
-export default function PageTitle(props) {
-	return <h2 className="page-title">{props.title}</h2>
-}
-
-PageTitle.propTypes = {
-	title: PropTypes.string.isRequired,
-}
